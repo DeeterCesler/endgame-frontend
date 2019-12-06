f2af682ff1126b88b607259eefbf4a18631d0031
Deeter <deeter.cesler@gmail.com>
2019-12-05 20:39:47 -0700

    removed background

--- src/HelpPage/index.jsx
+++ src/HelpPage/index.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const HelpPage = () => {
     return(
-        <div className="background">
+        <div>
             <div className="mini-spacer"/>
             <div className="mini-spacer"/>
             <h1>Need help?</h1>
             <br/>
-            <h5>Send me an email! I always want to hear what you love, hate, or just would love to see next with FollowUp.</h5>
+            <h5>Send me an email! I always want to hear what you love, hate, or just would love to see next.</h5>
             <br/>
             <h5>You can reach me at <a href="mailto:deeter.cesler@gmail.com">deeter.cesler@gmail.com</a>.</h5>
         </div>
